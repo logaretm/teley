3abb0461ec8541df7136fed343f50e0d7b37f563
Abdelrahman Awad <logaretm1@gmail.com>
2025-11-16 18:59:23 +0200

    feat: use cached event handler for trace get

--- server/api/traces/index.get.ts
+++ server/api/traces/index.get.ts
@@ -1,13 +1,18 @@
 // API endpoint to fetch all traces
 
-export default defineEventHandler(async (event) => {
-  const query = getQuery(event);
-  const limit = query.limit ? parseInt(query.limit as string) : 100;
-  
-  const traces = await getTraces(limit);
-  
-  return {
-    traces
-  };
-});
+export default defineCachedEventHandler(
+  async (event) => {
+    const query = getQuery(event);
+    const limit = query.limit ? parseInt(query.limit as string) : 100;
 
+    const traces = await getTraces(limit);
+
+    return {
+      traces,
+    };
+  },
+  {
+    maxAge: 60 * 60 * 4, // 4 hours
+    swr: true,
+  },
+);
